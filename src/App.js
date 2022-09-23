ef703152e242643839127fc544381926176c7a89
gstar2013 <84575737+gstar2013@users.noreply.github.com>
2022-09-23 11:15:45 +0100

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './component/TodoInput';
 import TodoList from './component/TodoList';
-
+//showing vs-code github setup
 class App extends Component{
   render(){
     return(
